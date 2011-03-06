1474064007c03f0121eb47410074ea72d7859335
ogoldberg <ogoldberg@gmail.com>
2011-03-06 13:26:16 -0600

    initial commit

---
+++ js/slideshow_fade.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+        slideShow();
+ });
+
+function slideShow() {
+    var current = $('#photos .show');
+    var next = current.next().length ? current.next() :
+    current.parent().children(':first');
+
+    current.hide().removeClass('show');
+    next.fadeIn().addclass('show);
+
+    setTimeout(slideShow, 3000);
+}
